e9b8d5e90f506192420f01f563122ab98c07e941
Sean Bloomfield <sbloomfield@theonion.com>
2014-03-12 17:52:47 -0500

    added contentlist controller, proxying to local api

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,48 @@
 'use strict';
 
+var underscore = angular.module('underscore', []);
+underscore.factory('_', function() {
+  return window._; // assumes underscore has already been loaded on the page
+});
+
+var jquery = angular.module('jquery', []);
+jquery.factory('$', function() {
+  return window.$; // assumes jquery has already been loaded on the page
+});
+
 angular.module('bulbsCmsApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ui.bootstrap',
+  'jquery',
+  'underscore'
 ])
-  .config(function ($routeProvider) {
+  .config(function ($locationProvider, $routeProvider, $sceProvider) {
+    $locationProvider.html5Mode(true);
+
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+      .when('/cms/app/list/:queue/', {
+        templateUrl: '/views/contentlist.html',
+        controller: 'ContentlistCtrl'
+      })
+      .when('/cms/app/edit/:id/', {
+        templateUrl: '/views/contentedit.html',
+        controller: 'ContenteditCtrl'
       })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/cms/app/list/published/'
       });
+
+    //TODO: whitelist staticonion.
+    $sceProvider.enabled(false);
+    /*.resourceUrlWhitelist([
+    'self',
+    STATIC_URL + "**"]);*/
+
+  })
+  .run(function($rootScope, $http, $cookies){
+    // set the CSRF token here
+    $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
   });
