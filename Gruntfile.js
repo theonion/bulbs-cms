07e09b13d17aecc6a00da6626020963f4118a002
Chris Sinchok <csinchok@theonion.com>
2014-03-20 15:02:42 -0500

    Let's serve on a different URL

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,9 +60,9 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: 9069,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
