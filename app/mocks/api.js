966a60857fef8f28f577a93be2f276b973b2cf74
Adam Wentz <awentz@theonion.com>
2014-04-08 12:15:27 -0500

    Mocking API responses

--- app/mocks/api.js
+++ app/mocks/api.js
@@ -1,60 +1,128 @@
-angular.module('bulbsCmsAppDev').run(function($httpBackend) {
-    //GETS
-    $httpBackend.whenGET(/^\/cms\/api\/v1\/content\/\d+\//).respond(function(method, url, data){
-      var re = /^\/cms\/api\/v1\/content\/(\d+)\//
+angular.module('bulbsCmsApp.mockApi').run([
+  '$httpBackend', 'mockApiData',
+  function($httpBackend, mockApiData) {
+    // content list
+    $httpBackend.whenGET('/cms/api/v1/content/').respond(mockApiData['content.list']);
+    
+    // content instance
+    $httpBackend.whenGET(/^\/cms\/api\/v1\/content\/\d+\/$/).respond(function(method, url, data) {
+      var re = /^\/cms\/api\/v1\/content\/(\d+)\//;
       var index = re.exec(url)[1];
-      if(index <= MOCK_content.results.length){
-
-        return [200, MOCK_content.results[index - 1], {}, "You did it"];
-      }else{
-        return [404, "Error body", {}, "You blew it"]; //todo: mock this correctly
+      var contentList = mockApiData['content.list'];
+      if(index <= contentList.results.length) {
+        return [200, contentList.results[index - 1]];
+      } else {
+        return [404, {"detail": "Not found"}];
       }
     });
+    $httpBackend.whenPOST('/cms/api/v1/content/', mockApiData['content.create'])
+      .respond(function(method, url, data) {
+        return [201, mockApiData['content.create.response']];
+      });
+    $httpBackend.whenPUT('/cms/api/v1/content/4/', mockApiData['content.edit'])
+      .respond(mockApiData['content.edit.response']);
 
-    $httpBackend.whenGET(/^\/cms\/api\/v1\/content.*/).respond(MOCK_content);
-    $httpBackend.whenGET(/^\/cms\/api\/v1\/things.*/).respond(MOCK_things);
-
-    //POSTS
-    //TODO: make this work
-    $httpBackend.whenPOST(/^\/cms\/api\/v1\/content/).respond(function(method, url, data) {
-      MOCK_content.results.push(angular.fromJson(data));
-    });
-
-    //pass through for templates
+    // things
+    $httpBackend.whenGET('/cms/api/v1/things/').respond(mockApiData['things.list']);
+    
+    // templates
     $httpBackend.whenGET(/^\/views\//).passThrough();
     $httpBackend.whenGET(/^\/content_type_views\//).passThrough();
-  });
-
-
-var MOCK_content = {
-  count: 100,
-  next: "/cms/api/v1/content/?page=2",
-  previous: null,
-  results: [
-    {
+  }
+]).constant('mockApiData', {
+  // NOTE: double-quotes are used because JSON
+  "content.create": {
+    "title": "A Test Article"
+  },
+  "content.create.response": {
+    "polymorphic_ctype": "core_article",
+    "tags": [],
+    "authors": [],
+    "image": null,
+    "absolute_url": "/article/a-test-article-1",
+    "detail_image": null,
+    "sponsor_image": null,
+    "status": "Draft",
+    "id": 4,
+    "published": null,
+    "last_modified": "2014-04-08T15:35:15.118Z",
+    "title": "A Test Article",
+    "slug": "a-test-article",
+    "description": "",
+    "feature_type": null,
+    "subhead": null,
+    "indexed": true,
+    "body": "",
+    "client_pixel": null,
+    "sponsor_name": null
+  },
+  "content.edit": {
+    "polymorphic_ctype": "core_article",
+    "tags": [],
+    "authors": [],
+    "image": null,
+    "absolute_url": "/article/a-test-article-1",
+    "detail_image": null,
+    "sponsor_image": null,
+    "status": "Draft",
+    "id": 4,
+    "published": null,
+    "last_modified": "2014-04-08T15:35:15.118Z",
+    "title": "A Test Article!!!",
+    "slug": "a-test-article-4",
+    "description": "",
+    "feature_type": null,
+    "subhead": null,
+    "indexed": true,
+    "body": "",
+    "client_pixel": null,
+    "sponsor_name": null
+  },
+  "content.edit.response": {
+    "polymorphic_ctype": "core_article",
+    "tags": [],
+    "authors": [],
+    "image": null,
+    "absolute_url": "/article/a-test-article-1",
+    "detail_image": null,
+    "sponsor_image": null,
+    "status": "Draft",
+    "id": 4, "published": null,
+    "last_modified": "2014-04-08T15:35:15.118Z",
+    "title": "A Test Article!!!",
+    "slug": "a-test-article-4",
+    "description": "",
+    "feature_type": null,
+    "subhead": null,
+    "indexed": true,
+    "body": "",
+    "client_pixel": null,
+    "sponsor_name": null
+  },
+  "content.list": {
+    count: 100,
+    next: "/cms/api/v1/content/?page=2",
+    previous: null,
+    results: [{
       polymorphic_ctype: "content_content",
-      tags: [
-        {
+      tags: [{
         slug: "section",
         type: "core_section",
         id: 1,
         name: "Section"
-        }
-      ],
-      authors: [
-        {
+      }],
+      authors: [{
         username: "username",
         first_name: "First",
         last_name: "Last",
         id: 1
-        }
-      ],
+      }],
       image: {
         caption: null,
         alt: null,
         id: "1"
       },
-      absolute_url: "/article/slug-10",
+      absolute_url: "/article/slug-1",
       detail_image: {
         caption: null,
         alt: null,
@@ -62,31 +130,26 @@ var MOCK_content = {
       },
       sponsor_image: null,
       status: "Draft",
-      id: 10,
+      id: 1,
       published: null,
       title: "This is a draft article",
       slug: "this-is-a-draft-article",
       feature_type: "Feature Type",
       body: "This is a draft article. It was written by First Last. It is a Feature Type article."
-    },
-    {
+    }, {
       polymorphic_ctype: "content_content",
-      tags: [
-        {
+      tags: [{
         slug: "film",
         type: "core_section",
         id: 22,
         name: "Film"
-        }
-      ],
-      authors: [
-        {
+      }],
+      authors: [{
         username: "milquetoast",
         first_name: "Milque",
         last_name: "Toast",
         id: 1
-        }
-      ],
+      }],
       image: {
         caption: null,
         alt: null,
@@ -100,11 +163,11 @@ var MOCK_content = {
       },
       sponsor_image: null,
       status: "Published",
-      id: 1,
+      id: 2,
       published: "2014-03-28T17:00:00Z",
       last_modified: "2014-03-27T19:13:04.074Z",
       title: "This is an article",
-      slug: "this-is-an-article",
+      slug: "this-is-an-article-2",
       description: "",
       feature_type: "Feature Type 1",
       subhead: "",
@@ -112,35 +175,32 @@ var MOCK_content = {
       body: "This is a body",
       client_pixel: null,
       sponsor_name: null
-    },
-    {
-      id: 2,
+    }, {
+      id: 3,
       polymorphic_ctype: "content_content",
       feature_type: "What A Gal",
       title: "This is another article",
-      authors: [
-        {
-          username: "BobbyNutson",
-          first_name: "Bobby",
-          last_name: "Nutson"
-        }
-      ],
+      slug: "this-is-another-article-3",
+      authors: [{
+        username: "BobbyNutson",
+        first_name: "Bobby",
+        last_name: "Nutson"
+      }],
       image: {
         id: "1"
       }
-    }
+    }]
+  },
+  "things.list": [
+    {"url": "/search?tags=so-you-think-you-can-dance", "param": "tags", "type": "tag", "name": "So You Think You Can Dance", "value": "so-you-think-you-can-dance"},
+    {"url": "/search?feature_types=oscar-this", "param": "feature_types", "type": "feature_type", "name": "Oscar This", "value": "oscar-this"},
+    {"url": "/search?feature_types=hear-this", "param": "feature_types", "type": "feature_type", "name": "Hear This", "value": "hear-this"},
+    {"url": "/search?feature_types=why-do-i-own-this", "param": "feature_types", "type": "feature_type", "name": "Why Do I Own This?", "value": "why-do-i-own-this"},
+    {"url": "/search?feature_types=out-this-month", "param": "feature_types", "type": "feature_type", "name": "Out This Month", "value": "out-this-month"},
+    {"url": "/search?feature_types=emmy-this", "param": "feature_types", "type": "feature_type", "name": "Emmy This!", "value": "emmy-this"},
+    {"url": "/search?feature_types=this-was-pop", "param": "feature_types", "type": "feature_type", "name": "This Was Pop", "value": "this-was-pop"},
+    {"url": "/search?feature_types=watch-this", "param": "feature_types", "type": "feature_type", "name": "Watch This", "value": "watch-this"},
+    {"url": "/search?feature_types=what-are-you-playing-this-weekend", "param": "feature_types", "type": "feature_type", "name": "What Are You Playing This Weekend?", "value": "what-are-you-playing-this-weekend"},
+    {"url": "/search?feature_types=i-watched-this-on-purpose", "param": "feature_types", "type": "feature_type", "name": "I Watched This On Purpose", "value": "i-watched-this-on-purpose"}
   ]
-}
-
-var MOCK_things = [
-  {"url": "/search?tags=so-you-think-you-can-dance", "param": "tags", "type": "tag", "name": "So You Think You Can Dance", "value": "so-you-think-you-can-dance"},
-  {"url": "/search?feature_types=oscar-this", "param": "feature_types", "type": "feature_type", "name": "Oscar This", "value": "oscar-this"},
-  {"url": "/search?feature_types=hear-this", "param": "feature_types", "type": "feature_type", "name": "Hear This", "value": "hear-this"},
-  {"url": "/search?feature_types=why-do-i-own-this", "param": "feature_types", "type": "feature_type", "name": "Why Do I Own This?", "value": "why-do-i-own-this"},
-  {"url": "/search?feature_types=out-this-month", "param": "feature_types", "type": "feature_type", "name": "Out This Month", "value": "out-this-month"},
-  {"url": "/search?feature_types=emmy-this", "param": "feature_types", "type": "feature_type", "name": "Emmy This!", "value": "emmy-this"},
-  {"url": "/search?feature_types=this-was-pop", "param": "feature_types", "type": "feature_type", "name": "This Was Pop", "value": "this-was-pop"},
-  {"url": "/search?feature_types=watch-this", "param": "feature_types", "type": "feature_type", "name": "Watch This", "value": "watch-this"},
-  {"url": "/search?feature_types=what-are-you-playing-this-weekend", "param": "feature_types", "type": "feature_type", "name": "What Are You Playing This Weekend?", "value": "what-are-you-playing-this-weekend"},
-  {"url": "/search?feature_types=i-watched-this-on-purpose", "param": "feature_types", "type": "feature_type", "name": "I Watched This On Purpose", "value": "i-watched-this-on-purpose"}
-]
+});
