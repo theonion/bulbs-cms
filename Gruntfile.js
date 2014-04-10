3a17fde52ddf660a003e42f5ea721d8228bbf447
Shaker Islam <shakeratheonion@gmail.com>
2014-04-10 16:39:14 -0500

    serve-lr

--- Gruntfile.js
+++ Gruntfile.js
@@ -408,7 +408,7 @@ module.exports = function (grunt) {
     ]);
   });
 
-  grunt.registerTask('serve', function (target) {
+  grunt.registerTask('serve-lr', function (target) {
     if (target === 'dist') {
       return grunt.task.run(['build', 'connect:dist:keepalive']);
     }
