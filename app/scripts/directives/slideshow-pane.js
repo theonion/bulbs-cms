401a56e61fb56703a6dee3ee4fcf1557bcc501a1
Shaker Islam <shakeratheonion@gmail.com>
2014-04-15 16:14:46 -0500

    splitting up directives, and just better organization overall.

--- app/scripts/directives/slideshow-pane.js
+++ app/scripts/directives/slideshow-pane.js
@@ -6,7 +6,9 @@ angular.module('bulbsCmsApp')
       restrict: 'E',
       templateUrl: routes.PARTIALS_URL + 'slideshow-pane.html',
       scope: {
-        article: '='
+        article: '=',
+        image: '=',
+        index: '='
       },
       link: function (scope, element, attrs) {
         var $element = $(element);
@@ -14,40 +16,13 @@ angular.module('bulbsCmsApp')
         if (attrs.caption === 'false') { scope.hideCaption = true; }
         scope.format = attrs.format || 'jpg';
         scope.crop = attrs.crop || '16x9';
-        scope.placeholderText = attrs.placeholderText || 'Optional Image';
 
-        scope.removeTitleImage = function () {
-          scope.image = null;
+        scope.removeImage = function (index) {
+          scope.article.slides.splice(index, 1);
         },
-        scope.addTitleImage = function () {
-          $window.uploadImage({
-            onSuccess: function (data) {
-              scope.$apply(function () {
-                scope.image = {
-                  id: data.id.toString(),
-                  alt: null,
-                  caption: null
-                };
-                setTimeout($window.picturefill, 200);
-                var el = $compile(
-                  "<slideshow-pane placeholder-text='Slide Image'></slideshow-pane>"
-                )( scope );
-                $element.parent().append( el );
-              });
-            },
-            onError: function (data) {
-              scope.$apply(function () {
-                alert('Error: ', data);
-              });
-            },
-            onProgress: function (data) {
-
-            }
-          });
-        },
-        scope.editTitleImage = function () {
+        scope.editImage = function (index) {
           $window.openImageDrawer(
-            scope.image.id,
+            scope.article.slides[index].id,
             function (data) {
               function removeLoadingGif() {
                 $element.find('.image img[src=\"' + routes.LOADING_IMG_SRC + '\"]').remove();
@@ -62,14 +37,14 @@ angular.module('bulbsCmsApp')
               $element.find('.image img').on('load', removeLoadingGif);
               $element.find('.image img').after('<img src=\"' + routes.LOADING_IMG_SRC + '\">');
 
-              scope.image.id = data.id.toString();
+              scope.article.slides[index].id = data.id.toString();
               scope.$apply();
               $window.picturefill();
               if ($element.find('.image img')[0].complete) { removeLoadingGif(); }
             },
             function () { return; },
             function (oldImage) {
-              scope.image = oldImage;
+              scope.article.slides[index] = oldImage;
               $window.picturefill();
             }
           );
