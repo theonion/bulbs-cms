89d2884880636a386e325dfdde19f227af9cea6f
Sean Bloomfield <sbloomfield@theonion.com>
2014-05-09 12:21:53 -0500

    last modified guard copy edits

--- app/scripts/controllers/lastmodifiedguardmodal.js
+++ app/scripts/controllers/lastmodifiedguardmodal.js
@@ -1,8 +1,16 @@
 'use strict';
 
 angular.module('bulbsCmsApp')
-  .controller('LastmodifiedguardmodalCtrl', function ($scope, $route, $modalInstance, articleOnPage, articleOnServer) {
+  .controller('LastmodifiedguardmodalCtrl', function ($scope, $route, $modalInstance, ContentApi, articleOnPage, articleOnServer) {
     $scope.articleOnServer = articleOnServer;
+
+    ContentApi.all('log').getList({content: article.id}).then(function (log) {
+      var latest = _.max(log, function(entry){ return moment(entry.action_time) })
+      var lastSavedById = latest.user;
+      ContentApi.one('author', lastSavedById).get().then(function (data) {
+        $scope.lastSavedBy = data;
+      });
+    });
 
     $scope.loadFromServer = function () {
       $route.reload();
