e0629a17d0b65c4aebea6f24247f362dee4c1ddd
Shaker Islam <shakeratheonion@gmail.com>
2014-05-27 11:49:20 -0500

    only show PNotify on 500 errors

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -77,36 +77,38 @@ angular.module('bulbsCmsApp', [
   $httpProvider.interceptors.push(function ($q, $window, PNotify) {
     return {
       responseError: function (rejection) {
-        var stack = {
-          animation: true,
-          dir1: 'up',
-          dir2: 'left'
-        };
-        new PNotify({
-          title: 'You found a bug!',
-          text:
-            'Looks like something just went wrong, and we need your help to fix it! \
-            Report it, and we\'ll make sure it never happens again.',
-          type: 'error',
-          confirm: {
-            confirm: true,
-            align: 'left',
-            buttons: [{
-              text: 'Report Bug',
-              addClass: 'btn-danger pnotify-report-bug',
-              click: function (notice) {
-                notice.remove();
-                $window.showBugReportModal(); // see bugreporter.js
-              }
-            }, {addClass: 'hidden'}] // removing the "Cancel" button
-          },
-          buttons: {
-            sticker: false
-          },
-          icon: 'fa fa-bug pnotify-error-icon',
-          addclass: "stack-bottomright",
-          stack: stack
-        });
+        if (rejection.status >= 500) {
+          var stack = {
+            animation: true,
+            dir1: 'up',
+            dir2: 'left'
+          };
+          new PNotify({
+            title: 'You found a bug!',
+            text:
+              'Looks like something just went wrong, and we need your help to fix it! \
+              Report it, and we\'ll make sure it never happens again.',
+            type: 'error',
+            confirm: {
+              confirm: true,
+              align: 'left',
+              buttons: [{
+                text: 'Report Bug',
+                addClass: 'btn-danger pnotify-report-bug',
+                click: function (notice) {
+                  notice.remove();
+                  $window.showBugReportModal(); // see bugreporter.js
+                }
+              }, {addClass: 'hidden'}] // removing the "Cancel" button
+            },
+            buttons: {
+              sticker: false
+            },
+            icon: 'fa fa-bug pnotify-error-icon',
+            addclass: "stack-bottomright",
+            stack: stack
+          });
+        }
         return $q.reject(rejection);
       }
     };
