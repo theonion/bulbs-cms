dc9d50f6216c41573f5d9f29d70280ecb84590aa
Shaker Islam <shakeratheonion@gmail.com>
2014-05-30 14:44:48 -0500

    Creating a nav directive that gets a templateUrl based on element attributes or app config

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -3,7 +3,6 @@
 angular.module('bulbsCmsApp')
   .controller('NavCtrl', function ($scope, ContentApi, routes) {
     $scope.PARTIALS_URL = routes.PARTIALS_URL;
-    $scope.NAV_LOGO = routes.NAV_LOGO;
 
     ContentApi.one('me').get().then(function(data){
       $scope.current_user = data;
---
+++ app/scripts/directives/nav.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('bulbsCmsApp')
+  .directive('nav', function (routes, navbar_options) {
+    return {
+      restrict: 'E',
+      templateUrl: function (tElement, tAttrs) {
+        var getCustomTemplate = navbar_options[tAttrs.view];
+
+        if (getCustomTemplate) {
+          return routes.DIRECTIVE_PARTIALS_URL + navbar_options[tAttrs.view] + '.html';
+        } else {
+          return routes.PARTIALS_URL + tAttrs.view + '.html';
+        }
+
+      },
+      scope: {},
+      link: function (scope, element, attrs) {
+        scope.NAV_LOGO = routes.NAV_LOGO;
+      }
+    };
+  });
