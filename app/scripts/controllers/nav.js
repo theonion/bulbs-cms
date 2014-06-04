1345c5a8308bb962cbea9d4bb5cc48e26d06d49b
Shaker Islam <shakeratheonion@gmail.com>
2014-06-04 11:51:31 -0500

    adding loggedInUser directive and CurrentUser service, and a little refactoring

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular.module('bulbsCmsApp')
-  .controller('NavCtrl', function ($scope, ContentApi, routes) {
-    $scope.PARTIALS_URL = routes.PARTIALS_URL;
+  .controller('NavCtrl', function ($scope, routes) {
 
-    ContentApi.one('me').get().then(function(data){
-      $scope.current_user = data;
-    })
   });
