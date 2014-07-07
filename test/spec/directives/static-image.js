5bce720c067548d08194d108e052e6bcf56cd59e
Sean Bloomfield <sbloomfield@theonion.com>
2014-07-07 11:15:52 -0500

    errant test

--- test/spec/directives/static-image.js
+++ test/spec/directives/static-image.js
@@ -12,9 +12,5 @@ describe('Directive: staticImage', function () {
     scope = $rootScope.$new();
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<static-image></static-image>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the staticImage directive');
-  }));
+
 });
