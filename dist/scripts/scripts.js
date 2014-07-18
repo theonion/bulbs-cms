b1f669fd4079c97ed622875c93db2522ca2bf90d
Sean Bloomfield <sbloomfield@theonion.com>
2014-07-18 15:00:10 -0500

    new build

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -955,8 +955,8 @@ angular.module('bulbsCmsApp')
         $scope.lastSavedPromotedArticles = _.clone(data.content);
         $scope.promotedArticles = data.content;
         $('.save-button').html(oldSaveHtml);
-      }, function(data, status){
-        if(status === 403){
+      }, function(data){
+        if(data.status === 403){
           Login.showLoginModal(data);
         }
       });
