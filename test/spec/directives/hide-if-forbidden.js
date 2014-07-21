51cb901edd93f257ce7ac5206e6d3e0be9e8af90
Sean Bloomfield <sbloomfield@theonion.com>
2014-07-21 17:00:46 -0500

    testing hide-if-forbidden

--- test/spec/directives/hide-if-forbidden.js
+++ test/spec/directives/hide-if-forbidden.js
@@ -4,17 +4,49 @@ describe('Directive: hideIfForbidden', function () {
 
   // load the directive's module
   beforeEach(module('bulbsCmsApp'));
+  beforeEach(module('jsTemplates'));
 
   var element,
-    scope;
+    scope,
+    httpBackend,
+    html,
+    optionsUrl403,
+    optionsUrl200;
 
-  beforeEach(inject(function ($rootScope) {
+  optionsUrl403 = '/return-a-403/';
+  optionsUrl200 = '/return-a-200/';
+
+  html = '<div hide-if-forbidden options-url="{{OPTIONSURL}}">This might be hidden!</div>';
+
+  beforeEach(inject(function ($rootScope, $httpBackend) {
     scope = $rootScope.$new();
+    httpBackend = $httpBackend;
+
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<hide-if-forbidden></hide-if-forbidden>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the hideIfForbidden directive');
+  it('should make a not-forbidden element visible', inject(function ($compile) {
+    httpBackend.expect('OPTIONS', optionsUrl200).respond(function(){
+      return [200, {detail: "Great Job"}];
+    });
+
+    var thisHtml = html.replace("{{OPTIONSURL}}", optionsUrl200)
+    element = angular.element($compile(thisHtml)(scope));
+
+    httpBackend.flush();
+
+    expect(element.css('visibility')).not.toBe('hidden');
+  }));
+
+  it('should make a forbidden element invisible', inject(function ($compile) {
+    httpBackend.expect('OPTIONS', optionsUrl403).respond(function(){
+      return [403, {detail: "Denied"}];
+    });
+
+    var thisHtml = html.replace("{{OPTIONSURL}}", optionsUrl403);
+    element = angular.element($compile(thisHtml)(scope));
+
+    httpBackend.flush();
+
+    expect(element.css('visibility')).toBe('hidden');
   }));
 });
