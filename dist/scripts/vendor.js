0c90f11865ff6e31662de3ea8f086dc771fad2d3
Michael Wnuk <mwnuk@theonion.com>
2014-07-28 15:16:21 -0500

    new build

--- dist/scripts/vendor.js
+++ dist/scripts/vendor.js
@@ -56891,12 +56891,14 @@ define('scribe-plugin-onion-video',[],function () {
 
 
       function insert(callback) {
+        
+        //TODO: Show some kind of use status while waiting for the initial response.
 
-        var activeElement = callback({video_id:"NONE"});
-        console.log("active video element", activeElement);
         return config.insertDialog().then(
             function(videoObject){
-                setVideoID(videoObject.attrs.id);
+              scribe.updateContents(function() {
+                callback({embed_url: config.videoEmbedUrl, video_id:videoObject.attrs.id});
+              });
             }, function(error){
                 onError(error);
             }, function(progress){
@@ -56908,12 +56910,6 @@ define('scribe-plugin-onion-video',[],function () {
             //update an indicator
         }
 
-        function setVideoID(id) {
-          scribe.updateContent(function() {
-            $("iframe", activeElement).attr("src", config.videoEmbedUrl + id);
-            $(activeElement).attr('data-video-id', id)
-          });
-        }
 
         function onError() {
             //show msg, allow user to trigger upload again
