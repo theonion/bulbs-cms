1b8978544aa50df8af3aa1ef09ddcc52e84fe188
Sean Bloomfield <sbloomfield@theonion.com>
2014-08-08 14:27:11 -0500

    Create lazy-include directive to only include templates when template would be visible. Create helper service Gettemplate to emulate ng-include behavior.

---
+++ app/scripts/services/gettemplate.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('bulbsCmsApp')
+  .service('Gettemplate', function Gettemplate($templateCache, $q, $http) {
+    this.get = function (templateUrl) {
+      var template = $templateCache.get(templateUrl);
+      if (template) {
+        return $q.when(template);
+      }else {
+        var deferred = $q.defer();
+        $http.get(templateUrl, {cache: true}).success(function (html) {
+          $templateCache.put(templateUrl, html);
+          deferred.resolve(html);
+        });
+        
+        return deferred.promise;
+      }
+    }
+  });
