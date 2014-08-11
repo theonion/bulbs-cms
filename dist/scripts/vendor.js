2de57d92f5a5e73e8b9c4690ed484701db42abcf
Adam Wentz <awentz@theonion.com>
2014-08-11 14:56:40 -0500

    new build

--- dist/scripts/vendor.js
+++ dist/scripts/vendor.js
@@ -57245,7 +57245,7 @@ define('link-formatter',[
         if (
             url.substr(0, 7) !== "http://" &&
             url.substr(0, 8) !== "https://" &&
-            url.substr(0, 6) !== "mailto:" &&
+            url.substr(0, 7) !== "mailto:" &&
             url.substr(0, 1) !== "/" 
             ) {
             // check for email, but default to http
