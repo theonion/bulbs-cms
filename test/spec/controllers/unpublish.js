0409e0c8a4440160eb148582289d24f179b6b5a0
Sean Bloomfield <sbloomfield@theonion.com>
2014-08-15 11:50:36 -0500

    test party

--- test/spec/controllers/unpublish.js
+++ test/spec/controllers/unpublish.js
@@ -6,14 +6,28 @@ describe('Controller: UnpublishCtrl', function () {
   beforeEach(module('bulbsCmsApp'));
 
   var UnpublishCtrl,
-    scope;
+    scope,
+    httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
     scope = $rootScope.$new();
+    httpBackend = $httpBackend;
     UnpublishCtrl = $controller('UnpublishCtrl', {
       $scope: scope
     });
   }));
+  
+  afterEach (function () {
+    httpBackend.verifyNoOutstandingExpectation ();
+    httpBackend.verifyNoOutstandingRequest ();
+  });
+
+  it('should have a function unpublish that unpublishes the article', function () {
+    scope.article = {id: 1};
+    httpBackend.expectPOST('/cms/api/v1/content/1/publish/').respond([200, {id: 1}]);
+    scope.unpublish();
+    httpBackend.flush()
+  });
 
 });
