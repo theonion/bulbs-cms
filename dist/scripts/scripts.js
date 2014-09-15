55044a42c824451173ba3090b64c107703740cdb
Andrew Kos <akos@theonion.com>
2014-09-15 10:29:49 -0500

    new build

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -423,11 +423,34 @@ angular.module('bulbsCmsApp')
 
       $scope.last_saved_article = angular.copy(data);
 
+      // log in to firebase
+      FirebaseApi.login();
+
       // register the current user as viewing this article
       FirebaseApi.registerCurrentUserActive($scope.article.id);
 
       // sync this article variable with the currently active users
-      $scope.activeUsers = FirebaseApi.getActiveUsers($scope.article.id);
+      FirebaseApi.getActiveUsers($scope.article.id, function (activeUsers) {
+
+//        $scope.activeUsers = activeUsers.$asArray();
+        var $activeUsers = activeUsers.$asArray();
+        $activeUsers.$watch(function () {
+
+          // do some unionizng on activeUsers data when it changes
+          $scope.activeUsers =
+            _.chain($activeUsers)
+              .groupBy(function (user) { return user.id; })
+              .map(function (group) {
+                var groupedUser = group[0];
+                groupedUser.count = group.length;
+                return groupedUser;
+              })
+              .sortBy(function (user) { return user.displayName; })
+              .value();
+
+        });
+
+      });
 
     };
 
@@ -2977,50 +3000,123 @@ angular.module('bulbsCmsApp')
 'use strict';
 
 angular.module('bulbsCmsApp')
-    .factory('FirebaseApi', function ($firebase, CurrentUser, firebaseApiConfig) {
+  .value('FIREBASE_URL', 'https://luminous-fire-8340.firebaseio.com/')
+  .value('FIREBASE_ROOT', 'a-site-is-not-configured')
+    .factory('FirebaseApi', function ($firebase, CurrentUser, FIREBASE_URL, FIREBASE_ROOT) {
 
-        var ref = new Firebase(firebaseApiConfig.FIREBASE_URL + firebaseApiConfig.FIREBASE_ROOT);
+        // get firebase references
+        var rootRef = new Firebase(FIREBASE_URL + 'sites/' + FIREBASE_ROOT);
+
+        // authorization utility function
+        var authed = false;
+        var afterAuthQueue = [];
+        var authFn = function (fn, args, scope) {
+
+          if (!authed) {
+
+            // not authorized yet, keep track of functions to call when we are authed
+            afterAuthQueue.push({
+              fn: fn,
+              args: args,
+              scope: scope
+            });
+
+          } else {
+
+            // we are authorized, just call the function normally
+            fn.apply(scope, args);
+
+          }
+
+        };
+
+        // ensure user is unauthed when they disconnect
+        var connectedRef = new Firebase(FIREBASE_URL + '.info/connected');
+        connectedRef.on('value', function (connected) {
+
+          if (!connected.val()) {
+
+            // user is no longer connected, unauthorize them from the server
+            rootRef.unauth();
+
+          }
+
+        });
+
+        var buildDisplayName = function () {
+
+          // try to show first/last name, if not try email, then username
+          return CurrentUser.data.first_name && CurrentUser.data.last_name
+                      ? CurrentUser.data.first_name + ' ' + CurrentUser.data.last_name
+                        : (CurrentUser.data.email || CurrentUser.data.username);
+
+        };
 
         return {
 
-            /**
-             * Return a firebase array of all users currently viewing a given article.
-             *
-             * @param articleId     ID of article to get users for.
-             * @returns {*}         AngularFire $FirebaseArray object.
-             */
-            getActiveUsers: function (articleId) {
+            login: function () {
 
-                return $firebase(ref.child('articles/' + articleId + '/users')).$asArray();
+              // just skip this whole thing if we're already authed
+              if (!authed && 'firebase_token' in CurrentUser.data && CurrentUser.data.firebase_token) {
+
+                // authorize user
+                rootRef.auth(CurrentUser.data.firebase_token, function (error) {
+
+                  if (error) {
+
+                    // authorization failed, log an error
+                    console.error('Firebase login failed:', error);
+
+                  } else {
+
+                    // user now authorized, call all the queued functions
+                    authed = true;
+                    for (var i = 0; i < afterAuthQueue.length; i++) {
+
+                      var param = afterAuthQueue[i];
+                      authFn(param.fn, param.args, param.scope);
+
+                    }
+
+                  }
+
+                });
+
+              }
 
             },
 
-            /**
-             * Register a user as viewing given article.
-             *
-             * @param articleId     ID of article that user is viewing.
-             */
+            getActiveUsers: function (articleId, callback) {
+
+              // use auth wrapper to ensure this function succeeds
+              authFn(function (callbackArg) {
+
+                callbackArg($firebase(rootRef.child('articles/' + articleId + '/users')));
+
+              }, [callback], this);
+
+            },
+
             registerCurrentUserActive: function (articleId) {
 
-                if ('id' in CurrentUser.data) {
+              // TODO : make sure firebase_token is avaiable
 
-                    // try to show first/last name, if not try username, then email
-                    var displayName =
-                            CurrentUser.data.first_name && CurrentUser.data.last_name
-                                ? CurrentUser.data.first_name + ' ' + CurrentUser.data.last_name
-                                    : (CurrentUser.data.username || CurrentUser.data.email);
+              // get active users then add current user
+              this.getActiveUsers(articleId, function (activeUsers) {
 
-                    this.getActiveUsers(articleId).$add({
-                        id: CurrentUser.data.id,
-                        fullName: displayName
-                    }).then(function (ref) {
+                  var rawUser = {
+                    id: CurrentUser.data.id,
+                    displayName: buildDisplayName()
+                  };
 
-                        // ensure user is removed once they leave this article
-                        ref.onDisconnect().remove();
+                  activeUsers.$asArray().$add(rawUser).then(function (userRef) {
 
-                    });
+                    // ensure user is removed on disconnect
+                    userRef.onDisconnect().remove();
 
-                }
+                  });
+
+              });
 
             }
 
