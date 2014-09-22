4d1c5ad5bb5f6318f477dcba2db5081fb67da1a9
Andrew Kos <akos@theonion.com>
2014-09-22 11:35:52 -0500

    Build

--- dist/scripts/vendor.js
+++ dist/scripts/vendor.js
@@ -67460,6 +67460,7 @@ define('scribe-plugin-inline-objects',[],function () {
         });
 
         scribe.el.parentNode.addEventListener('mouseleave', function (event) {
+          hideToolbar();
           $('.embed-tools', editorEl).removeClass('active');
         });
 
@@ -68206,6 +68207,74 @@ define('paste-sanitize',['scribe-common/src/element'], function (scribeElement) 
 
 });
 
+define('remove-a-styles',['scribe-common/src/element'], function (scribeElement) {
+
+  
+
+  return function () {
+    return function (scribe) {
+
+      function traverse(parentNode) {
+        var node = parentNode.firstElementChild;
+
+        while (node) {
+          if (node.nodeName === 'A' && node.hasAttribute('style')) {
+            node.removeAttribute('style');
+          }
+          else if (node.children.length > 0) {
+            traverse(node);
+          }
+          node = node.nextElementSibling;
+        }
+      }
+
+      scribe.registerHTMLFormatter('sanitize', function (html) {
+
+        var bin = document.createElement('div');
+        bin.innerHTML = html;
+
+        traverse(bin);
+        return bin.innerHTML;
+      });
+    };
+  };
+
+});
+
+define('strip-bold-in-headings',['scribe-common/src/element'], function (scribeElement) {
+
+  
+
+  return function () {
+    return function (scribe) {
+
+      function traverse(parentNode) {
+        var node = parentNode.firstElementChild;
+
+        while (node) {
+          if (node.nodeName === 'B' && (/^(H[1-6])$/).test(parentNode.nodeName)) {
+            scribeElement.unwrap(parentNode, node);
+          }
+          else if (node.children.length > 0) {
+            traverse(node);
+          }
+          node = node.nextElementSibling;
+        }
+      }
+
+      scribe.registerHTMLFormatter('sanitize', function (html) {
+
+        var bin = document.createElement('div');
+        bin.innerHTML = html;
+
+        traverse(bin);
+        return bin.innerHTML;
+      });
+    };
+  };
+
+});
+
 define('our-ensure-selectable-containers',[
     'scribe-common/src/element',
     'lodash-amd/modern/collections/contains'
@@ -68422,6 +68491,8 @@ define('onion-editor',[
   'paste-strip-nbsps',
   'paste-from-word',
   'paste-sanitize',
+  'remove-a-styles',
+  'strip-bold-in-headings',
   // scribe core
   'our-ensure-selectable-containers',
   'enforce-p-elements'
@@ -68450,6 +68521,8 @@ define('onion-editor',[
   pasteStripNbsps,
   pasteFromWord,
   pasteSanitize,
+  removeAStyles,
+  stripBoldInHeadings,
   // scribe core
   ourEnsureSelectableContainers,
   enforcePElements
@@ -68492,6 +68565,46 @@ define('onion-editor',[
       scribe.use(enforcePElements());
       scribe.use(ourEnsureSelectableContainers({skipElement: skipSanitization}));
     }
+
+    // MO' HACKZ: We don't want to kill SPANS inside our inline divs
+    var insertHTMLCommandPatch = new scribe.api.CommandPatch('insertHTML');
+    insertHTMLCommandPatch.execute = function (value) {
+      scribe.transactionManager.run(function () {
+        scribe.api.CommandPatch.prototype.execute.call(this, value);
+
+        sanitize(scribe.el);
+
+        function sanitize(parentNode) {
+          var treeWalker = document.createTreeWalker(parentNode, NodeFilter.SHOW_ELEMENT);
+          var node = treeWalker.firstChild();
+          if (!node) { return; }
+
+          do {
+            if (node.nodeName === 'SPAN' && node.className.indexOf('inline') === -1) {
+              element.unwrap(parentNode, node);
+            } else {
+              /**
+               * If the list item contains inline elements such as
+               * A, B, or I, Chrome will also append an inline style for
+               * `line-height` on those elements, so we remove it here.
+               */
+              node.style.lineHeight = null;
+
+              // There probably wasn’t a `style` attribute before, so
+              // remove it if it is now empty.
+              if (node.getAttribute('style') === '') {
+                node.removeAttribute('style');
+              }
+            }
+
+            // Sanitize children
+            sanitize(node);
+          } while ((node = treeWalker.nextSibling()));
+        }
+      }.bind(this));
+    };
+    scribe.commandPatches.insertHTML = insertHTMLCommandPatch;
+
     // ENDHACK
 
     if (options.placeholder) {
@@ -68630,6 +68743,8 @@ define('onion-editor',[
       scribe.use(scribePluginEmbed());
       scribe.use(scribePluginHr());
       scribe.use(scribePluginOnionVideo(options.video));
+      scribe.use(removeAStyles());
+      scribe.use(stripBoldInHeadings());
     }
 
     scribe.use(scribePluginSanitizer({
