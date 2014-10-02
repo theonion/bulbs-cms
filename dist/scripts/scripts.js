5f007abb4c0bb285d1a4ef39facc67e4ff49b3c0
Andrew Kos <akos@theonion.com>
2014-10-02 16:36:32 -0500

    Updated dist

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -2930,9 +2930,15 @@ angular.module('bulbsCmsApp')
 
 angular.module('bulbsCmsApp')
   .factory('VersionBrowserModalOpener', function ($modal, routes) {
+    var modal = null;
     return {
       open: function ($scope, article) {
-        return $modal.open({
+        // ensure any version browser modals are closed before opening a new one
+        if (modal) {
+          $modal.close();
+        }
+
+        modal =  $modal.open({
           templateUrl: routes.PARTIALS_URL + 'modals/version-browser-modal.html',
           controller: 'VersionBrowserModalCtrl',
           scope: $scope,
@@ -2941,6 +2947,8 @@ angular.module('bulbsCmsApp')
             article: function () { return article; }
           }
         });
+
+        return modal;
       }
     };
   });
