c40848ec8a2d3e0fa2ad797607ee669a9dc1986c
Andrew Kos <akos@theonion.com>
2014-10-06 13:19:22 -0500

    CMS notification bar logic in place

--- app/scripts/controllers/cms-notify-bar.js
+++ app/scripts/controllers/cms-notify-bar.js
@@ -1,11 +1,33 @@
 'use strict';
 
 angular.module('bulbsCmsApp')
-  .controller('CmsNotifyBarCtrl', function ($scope, CmsNotificationsApi) {
+  .controller('CmsNotifyBarCtrl', function ($cookies, $scope, ipCookie, moment, CmsNotificationsApi) {
 
+    var genCookieKey = function (id) {
+      return 'dismissed-cms-notification-' + id;
+    };
+
+    // get list of notifications to show
     CmsNotificationsApi.getList().then(function (notifications) {
-// TODO : get list of notifications that should be visible
-      $scope.notifications = notifications;
+      var now = moment();
+      $scope.notifications = _.filter(notifications, function (notification) {
+        // show notifications where there is no dismiss cookie and post_date < now < notify_end_date
+        return !ipCookie(genCookieKey(notification.id))
+                && moment(notification.post_date).isBefore(now) && moment(notification.notify_end_date).isAfter(now);
+      });
     });
+
+    /**
+     * Dismiss a given notification. Stores a dismiss cookie which will expire one day after the notification's end
+     *  date.
+     *
+     * @param notification  Notification to dismiss.
+     */
+    $scope.dismissNotification = function (notification) {
+      var cookieKey = URLify(genCookieKey(notification.id));
+      ipCookie(cookieKey, true, {
+        expires: moment(notification.notify_end_date).add({days: 1}).diff(moment(), 'days')
+      });
+    };
 
   });
