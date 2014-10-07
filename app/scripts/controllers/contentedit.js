7f4e32d37d45aa82a7ceac58ad6bc11195356f2a
Andrew Kos <akos@theonion.com>
2014-10-07 15:44:27 -0500

    Save notification no longer hides automatically

--- app/scripts/controllers/contentedit.js
+++ app/scripts/controllers/contentedit.js
@@ -57,6 +57,7 @@ angular.module('bulbsCmsApp')
                       text: msg,
                       type: 'error',
                       mouse_reset: false,
+                      hide: false,
                       confirm: {
                         confirm: true,
                         buttons: [{
