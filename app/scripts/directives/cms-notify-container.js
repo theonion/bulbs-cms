f5ebe477b972f5053dc8219abedad9c0b8d39771
Andrew Kos <akos@theonion.com>
2014-10-13 14:19:09 -0500

    CMS notifications will now only show up on listing page, no longer using pnotify

---
+++ app/scripts/directives/cms-notify-container.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('bulbsCmsApp')
+  .directive('cmsNotifyContainer', function (routes) {
+    return {
+      restrict: 'E',
+      scope: {},
+      templateUrl: routes.PARTIALS_URL + 'cms-notify-container.html',
+      controller: 'CmsNotifyContainerCtrl'
+    }
+  });
