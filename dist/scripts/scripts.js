f1b450967590001f985e6b8e42e0032a61ff2831
Andrew Kos <akos@theonion.com>
2014-10-13 14:20:37 -0500

    Updated dist

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1688,61 +1688,39 @@ angular.module('bulbsCmsApp')
  * Controller for notifications bar that is displayed to users.
  */
 angular.module('bulbsCmsApp')
-  .controller('CmsNotifyPopupsCtrl', function ($scope, $window, ipCookie, moment, CmsNotificationsApi, PNotify) {
+  .controller('CmsNotifyContainerCtrl', function ($scope, $window, ipCookie, moment, CmsNotificationsApi, URLify) {
 
     var genCookieKey = function (id) {
           return 'dismissed-cms-notification-' + id;
         },
-        notify = function (notification) {
-          new PNotify({
-            title: 'New Update',
-            text: notification.title,
-            type: 'info',
-            mouse_reset: false,
-            hide: false,
-            confirm: {
-              confirm: true,
-              buttons: [{
-                text: 'Learn More',
-                addClass: 'btn-info',
-                click: function (notice) {
-                  notice.mouse_reset = false;
-                  notice.remove();
-
-                  // take user to notifications page
-                  $window.location.href = '/cms/app/notifications/'
-                }
-              }, {
-                addClass: 'hide'
-              }]
-            },
-            buttons: {
-              sticker: false,
-              closer_hover: false
-            },
-            before_close: function () {
-              // add dismiss cookie
-              var cookieKey = URLify(genCookieKey(notification.id));
-              ipCookie(cookieKey, true, {
-                expires: moment(notification.notify_end_date).add({days: 1}).diff(moment(), 'days'),
-                path: '/cms/app'
-              });
+        updateNotificationsDisplay = function (notifications) {
+          var now = moment();
+          $scope.notifications = _.filter(notifications, function (notification) {
+            // show notifications where there is no dismiss cookie and post_date < now < notify_end_date
+            if (!ipCookie(genCookieKey(notification.id))
+                && moment(notification.post_date).isBefore(now)
+                && moment(notification.notify_end_date).isAfter(now)) {
+              return true;
             }
           });
         };
 
     // show notifications
     CmsNotificationsApi.getList().then(function (notifications) {
-      var now = moment();
-      $scope.notifications = _.each(notifications, function (notification) {
-        // show notifications where there is no dismiss cookie and post_date < now < notify_end_date
-        if (!ipCookie(genCookieKey(notification.id))
-            && moment(notification.post_date).isBefore(now)
-            && moment(notification.notify_end_date).isAfter(now)) {
-          notify(notification);
-        }
-      });
+      updateNotificationsDisplay(notifications);
     });
+
+    $scope.dismissNotification = function (notification) {
+      // add dismiss cookie
+      var cookieKey = URLify(genCookieKey(notification.id));
+      ipCookie(cookieKey, true, {
+        expires: moment(notification.notify_end_date).add({days: 1}).diff(moment(), 'days'),
+        path: '/cms/app'
+      });
+
+      // hide notification
+      updateNotificationsDisplay($scope.notifications);
+    };
 
   });
 'use strict';
@@ -2962,11 +2940,12 @@ angular.module('bulbsCmsApp')
 'use strict';
 
 angular.module('bulbsCmsApp')
-  .directive('cmsNotifyPopups', function () {
+  .directive('cmsNotifyContainer', function (routes) {
     return {
       restrict: 'E',
       scope: {},
-      controller: 'CmsNotifyPopupsCtrl'
+      templateUrl: routes.PARTIALS_URL + 'cms-notify-container.html',
+      controller: 'CmsNotifyContainerCtrl'
     }
   });
 'use strict';
