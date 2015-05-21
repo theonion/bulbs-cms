4f23ceb98ccd1c3cd788dc0df3d35ff5b27c803c
Andrew Kos <akos@theonion.com>
2015-05-20 19:16:23 -0500

    Fix create urls

--- app/scripts/directives/create-content.js
+++ app/scripts/directives/create-content.js
@@ -52,7 +52,7 @@ angular.module('bulbsCmsApp')
         function saveArticle() {
           $('button.go').removeClass('btn-danger').addClass('btn-success').html('<i class="fa fa-refresh fa-spin"></i> Going');
           $http({
-            url: '/cms/api/v1/content/?doctype=' + $scope.contentType,
+            url: CmsConfig.buildBackendUrl('/cms/api/v1/content/?doctype=' + $scope.contentType),
             method: 'POST',
             data: $scope.init
           }).success(function (resp) {
