0fef2126bfa0901b3af19fa379c2efb226c9623e
Cameron Lowe <cam.m.lowe@gmail.com>
2015-09-09 14:33:45 -0500

    these should be variables

--- app/components/content/content-edit/content-edit-body/content-edit-body.js
+++ app/components/content/content-edit/content-edit-body/content-edit-body.js
@@ -6,7 +6,9 @@ angular.module('content.edit.body', [])
       restrict: 'E',
       scope: {
         article: '=',
-        inlineObjectsUrl: '@'
+        inlineObjectsUrl: '@',
+        linkDomain: '@',
+        searchHandler: '@'
       },
       templateUrl: COMPONENTS_URL + 'content/content-edit/content-edit-body/content-edit-body.html'
     };
