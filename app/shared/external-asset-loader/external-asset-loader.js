843d893214747156fc730833561a21cd3e65dbe6
Andrew Kos <akos@theonion.com>
2015-09-09 11:35:17 -0500

    Added external-asset-loader

---
+++ app/shared/external-asset-loader/external-asset-loader.js
@@ -0,0 +1,52 @@
+;(function (global) {
+  'use strict';
+
+  /**
+   * Insert assets from external sources, optionally cache busted.
+   */
+  global.ExternalAssetLoader = (function () {
+
+    var _checkCacheBust = function (url, cacheBust) {
+      return cacheBust ? url + '?' + (new Date()).valueOf() : url;
+    };
+
+    var _insert = function (element, type, insertLocation) {
+      var firstOfType = insertLocation.querySelectorAll(type);
+      insertLocation.insertBefore(element, firstOfType);
+    };
+
+    this.TYPES = {
+      STYLE: 'link',
+      SCRIPT: 'script'
+    };
+
+    this.addAsset = function (optsIn) {
+      var element;
+
+      if (typeof optsIn.url !== 'string') {
+        throw new Error('ExternalAssetLoader: Must define asset url string to load.');
+      } else if (typeof optsIn.type !== 'string') {
+        throw new Error('ExternalAssetLoader: Must define an asset type string to load.');
+      }
+
+      if (optsIn.type === this.TYPES.STYLE) {
+        element = document.createElement(optsIn.type);
+
+        element.href = _checkCacheBust(optsIn.url, optsIn.cacheBust);
+        element.rel = 'stylesheet';
+
+        _insert(element, optsIn.type, document.head);
+      } else if (optsIn.type === this.TYPES.SCRIPT) {
+        element = document.createElement(optsIn.type);
+
+        element.src = _checkCacheBust(optsIn.url, optsIn.cacheBust);
+
+        _insert(element, optsIn.type, document.head);
+      }
+
+      return element;
+    };
+
+    return this;
+  })();
+})(window);
