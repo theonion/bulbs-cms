d1edd89e17788413e2c5d7f31974c9f0e437947c
Mike Parent <mparent@umich.edu>
2016-02-03 10:29:42 -0600

    new build

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -4232,8 +4232,6 @@ angular.module('specialCoverage.edit.directive', [
 
         $scope.needsSave = false;
 
-        $scope.tunicCampaignIdMapping = {};
-
         var modelId = $scope.getModelId();
         if (modelId === 'new') {
           // this is a new special coverage, build it
@@ -4241,13 +4239,8 @@ angular.module('specialCoverage.edit.directive', [
           $scope.isNew = true;
         } else {
           // this is an existing special coverage, find it
-          $scope.model = SpecialCoverage.$find($scope.getModelId()).$then(function () {
-            $scope.model.$loadTunicCampaign().then(function (campaign) {
-              $scope.tunicCampaignIdMapping[campaign.id] = campaign;
-            });
-          });
+          $scope.model = SpecialCoverage.$find($scope.getModelId());
         }
-
 
         window.onbeforeunload = function (e) {
           if (!_.isEmpty($scope.model.$dirty()) || $scope.isNew || $scope.needsSave) {
@@ -4291,21 +4284,8 @@ angular.module('specialCoverage.edit.directive', [
           });
         };
 
-        $scope.tunicCampaignFormatter = function (campaignId) {
-          if (campaignId in $scope.tunicCampaignIdMapping) {
-            var campaign = $scope.tunicCampaignIdMapping[campaignId];
-            return campaign.name + ' - ' + campaign.number;
-          }
-        };
-
         $scope.searchCampaigns = function (searchTerm) {
-          return $scope.model.$searchCampaigns({search: searchTerm}).then(function (campaigns) {
-            campaigns.forEach(function (campaign) {
-              $scope.tunicCampaignIdMapping[campaign.id] = campaign;
-            });
-            // Formatter expects list of IDs
-            return campaigns.map(function (campaign) { return campaign.id; });
-          });
+          return Campaign.simpleSearch(searchTerm);
         };
       },
       restrict: 'E',
@@ -5175,11 +5155,10 @@ angular.module('apiServices.specialCoverage.factory', [
   'apiServices',
   'apiServices.campaign.factory',
   'apiServices.mixins.fieldDisplay',
-  'cms.tunic.config',
   'filters.moment',
   'VideohubClient.api'
 ])
-  .factory('SpecialCoverage', function (_, $http, $parse, $q, restmod, TunicConfig, Video) {
+  .factory('SpecialCoverage', function (_, $parse, restmod, Video) {
     var ACTIVE_STATES = {
       INACTIVE: 'Inactive',
       PROMOTED: 'Pin to HP'
@@ -5270,28 +5249,6 @@ angular.module('apiServices.specialCoverage.factory', [
           $loadVideosData: function () {
             _.each(this.videos, function (video) {
               video.$fetch();
-            });
-          },
-          /**
-           * Load campaign data from Tunic endpoint
-           */
-          $loadTunicCampaign: function () {
-            if (_.isNumber(this.tunicCampaignId)) {
-              return $http.get(TunicConfig.buildBackendApiUrl('campaign/' + this.tunicCampaignId + '/')).then(function (result) {
-                return result.data;
-              });
-            }
-            return $q.reject();
-          },
-          /**
-           * Load campaign search results from Tunic endpoint
-           */
-          $searchCampaigns: function (params) {
-
-            return $http.get(TunicConfig.buildBackendApiUrl('campaign/'), {
-              params: params,
-            }).then(function (response) {
-              return response.data.results;
             });
           },
           /**
