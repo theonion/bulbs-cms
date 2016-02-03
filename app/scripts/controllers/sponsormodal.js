e8ab63529de19ccb0d8f1200619a6fbd2d8c2a05
Mike Parent <mparent@umich.edu>
2016-02-03 16:01:53 -0600

    Lint

--- app/scripts/controllers/sponsormodal.js
+++ app/scripts/controllers/sponsormodal.js
@@ -3,7 +3,7 @@
 angular.module('bulbsCmsApp')
   .controller('SponsormodalCtrl', function ($scope, ContentFactory, article, Campaign,
                                             // NOTE: temp depdendencies will be moved along with searchCampaigns/loadCampaigns
-                                            $http, TunicConfig) {
+                                            _, $q, $http, TunicConfig) {
     $scope.article = article;
 
     $scope.tunicCampaignIdMapping = {};
@@ -18,14 +18,11 @@ angular.module('bulbsCmsApp')
       return $q.reject();
     };
 
-    $scope.searchCampaigns = function (searchTerm) {
-      // TODO: Move into model
-      return $searchCampaigns({search: searchTerm}).then(function (campaigns) {
-        campaigns.forEach(function (campaign) {
-          $scope.tunicCampaignIdMapping[campaign.id] = campaign;
-        });
-        // Formatter expects list of IDs
-        return campaigns.map(function (campaign) { return campaign.id; });
+    var $searchCampaigns = function (params) {
+      return $http.get(TunicConfig.buildBackendApiUrl('campaign/'), {
+        params: params,
+      }).then(function (response) {
+        return response.data.results;
       });
     };
     // --------------------------- END TODO ---------------------------------------
@@ -47,13 +44,15 @@ angular.module('bulbsCmsApp')
       }
     };
 
-    var $searchCampaigns = function (params) {
-      return $http.get(TunicConfig.buildBackendApiUrl('campaign/'), {
-        params: params,
-      }).then(function (response) {
-        return response.data.results;
+    $scope.searchCampaigns = function (searchTerm) {
+      return $searchCampaigns({search: searchTerm}).then(function (campaigns) {
+        campaigns.forEach(function (campaign) {
+          $scope.tunicCampaignIdMapping[campaign.id] = campaign;
+        });
+        // Formatter expects list of IDs
+        return campaigns.map(function (campaign) { return campaign.id; });
       });
-    }
+    };
 
     $scope.tunicCampaignFormatter = function (campaignId) {
       if (campaignId in $scope.tunicCampaignIdMapping) {
