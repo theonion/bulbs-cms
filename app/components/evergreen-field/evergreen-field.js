bead002deba65105b32b6a558b3b5d863e7254f6
Mike Parent <mparent@umich.edu>
2016-02-25 15:59:14 -0600

    Remove unused copy-pasta code

--- app/components/evergreen-field/evergreen-field.js
+++ app/components/evergreen-field/evergreen-field.js
@@ -3,12 +3,3 @@
 angular.module('evergreenField', [
   'evergreenField.directive'
 ]);
-  /*.value('TEMPLATE_TYPES', [{*/
-  /*    name: 'Small Width',*/
-  /*    slug: 'small-width',*/
-  /*    content_type: 'content_content'*/
-  /*  }, {*/
-  /*    name: 'Large Width',*/
-  /*    slug: 'large-width',*/
-  /*    content_type: 'content_content'*/
-  /*  }]);*/
