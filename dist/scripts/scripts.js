bb15c61d0fc5463c00b2be91da99a18f86f2d52a
Mike Parent <mparent@umich.edu>
2016-02-26 09:32:29 -0600

    new build

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -260,6 +260,7 @@ angular.module('bulbsCmsApp', [
   'bettyEditable',
   'bugReporter',
   'campaigns',
+  'evergreenField',
   'filterWidget',
   'filterListWidget',
   'polls',
@@ -1936,6 +1937,32 @@ angular.module('EditorsPick', [
         reloadOnSearch: false
       });
   });
+
+'use strict';
+
+angular.module('evergreenField.directive', [
+  'bulbsCmsApp.settings',
+  'lodash',
+  'saveButton.directive',
+])
+  .directive('evergreenField', [
+    'routes',
+    function (routes) {
+      return {
+        restrict: 'E',
+        scope: {
+          article: '='
+        },
+        templateUrl: routes.COMPONENTS_URL + 'evergreen-field/evergreen-field.html'
+      };
+    }
+  ]);
+
+'use strict';
+
+angular.module('evergreenField', [
+  'evergreenField.directive'
+]);
 
 'use strict';
 
