b0275a84ab497985b672de0608d6f8b182acd1ae
Collin Miller <collintmiller@gmail.com>
2016-03-08 10:35:00 -0600

    poll validation tests

--- app/components/polls/polls-edit/polls-edit-directive.tests.js
+++ app/components/polls/polls-edit/polls-edit-directive.tests.js
@@ -5,6 +5,7 @@ describe('Directive: pollsEdit', function () {
       $httpBackend,
       $routeParams,
       $rootScope,
+      $timeout,
       answer,
       element,
       html,
@@ -15,11 +16,12 @@ describe('Directive: pollsEdit', function () {
   beforeEach(module('bulbsCmsApp'));
   beforeEach(module('jsTemplates'));
 
-  beforeEach(inject(function(_$compile_, _$httpBackend_, _$routeParams_, _$rootScope_) {
+  beforeEach(inject(function(_$compile_, _$httpBackend_, _$routeParams_, _$rootScope_, _$timeout_) {
     $compile = _$compile_;
     $httpBackend = _$httpBackend_;
     $routeParams = _$routeParams_;
     $rootScope = _$rootScope_;
+    $timeout = _$timeout_;
 
     $routeParams.id = 'new';
 
@@ -95,6 +97,88 @@ describe('Directive: pollsEdit', function () {
       answer = scope.answers[0];
       scope.removeAnswer(answer.id);
       expect(scope.deletedAnswers.length).toEqual(0);
+    });
+  });
+
+  describe('validations', function () {
+    // These validation tests follow this format:
+
+    // Test invalid state
+    // Test valid state
+    it('requires question title', function () {
+      var input = element.find('input[name=title]');
+      input.val('').trigger('input');
+      expect(
+        element.find('label[for=pollTitle] .error-message.ng-hide').length
+      ).toBe(0);
+
+      input.val('anything').trigger('input');
+      expect(
+        element.find('label[for=pollTitle] .error-message.ng-hide').length
+      ).toBe(1);
+    });
+
+    it('requires question text', function () {
+      var input = element.find('textarea[name=question_text]');
+      input.val('').trigger('input');
+      expect(
+        element.find('label[for=pollQuestionText] .error-message.ng-hide').length
+      ).toBe(0);
+
+      input.val('anything').trigger('input');
+      expect(
+        element.find('label[for=pollQuestionText] .error-message.ng-hide').length
+      ).toBe(1);
+    });
+
+    it('requires answer text', function () {
+      var input = element.find('textarea[name=answer_text]:first');
+      input.val('').trigger('input');
+
+      expect(
+        element.find('label[for=answerText]:first .error-message.ng-hide').length
+      ).toBe(0);
+
+      input.val('anything').trigger('input');
+      expect(
+        element.find('label[for=answerText]:first .error-message.ng-hide').length
+      ).toBe(1);
+    });
+
+    it('polls with an end date require a published date', function () {
+      scope.model.end_date = moment.now();
+      scope.validatePublication();
+      $timeout.flush();
+      expect(
+        element.find('label[for=pollStartDate] .error-message.ng-hide').length
+      ).toBe(0);
+
+      scope.model.end_date = undefined;
+      scope.validatePublication();
+      $timeout.flush();
+      expect(
+        element.find('label[for=pollStartDate] .error-message.ng-hide').length
+      ).toBe(1);
+    });
+
+    it('requires end date to be after start date', function () {
+      scope.model.published = moment().add(1, 'day');
+      scope.model.end_date = moment();
+      scope.validatePublication();
+      $timeout.flush();
+      expect(
+        element.find('label[for=pollEndDate] .error-message.ng-hide').length
+      ).toBe(0);
+
+      scope.model.end_date = moment.now();
+
+      scope.model.published = moment();
+      scope.model.end_date = moment().add(1, 'day');
+      scope.validatePublication();
+      $timeout.flush();
+      expect(
+        element.find('label[for=pollEndDate] .error-message.ng-hide').length
+      ).toBe(1);
     });
   });
 });
