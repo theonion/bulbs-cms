571587a56be7d5c903c21d5b2b50633dd44ff396
Andrew Kos <akos123@gmail.com>
2016-05-31 15:04:18 -0500

    formatting

--- app/scripts/directives/static-image.js
+++ app/scripts/directives/static-image.js
@@ -1,23 +1,28 @@
 'use strict';
 
 angular.module('bulbsCmsApp')
-  .directive('staticImage', function (routes, STATIC_IMAGE_URL) {
-    return {
-      templateUrl: routes.PARTIALS_URL + 'static-image.html',
-      restrict: 'E',
-      scope: {
-        'image': '='
-      },
-      link: function postLink(scope, element, attrs) {
-        var ratio = attrs.ratio || '16x9';
+  .directive('staticImage', [
+    'routes', 'STATIC_IMAGE_URL',
+    function (routes, STATIC_IMAGE_URL) {
+      return {
+        templateUrl: routes.PARTIALS_URL + 'static-image.html',
+        restrict: 'E',
+        scope: {
+          image: '='
+        },
+        link: function (scope, element, attrs) {
+          var ratio = attrs.ratio || '16x9';
 
-        scope.$watch('image', function () {
-          if (scope.image && scope.image.id) {
-            scope.imageUrl = STATIC_IMAGE_URL.replace('{{ratio}}', ratio).replace('{{image}}', scope.image.id);
-          } else {
-            scope.imageUrl = false;
-          }
-        });
-      }
-    };
-  });
+          scope.$watch('image', function () {
+            console.log(scope.image)
+
+            if (scope.image && scope.image.id) {
+              scope.imageUrl = STATIC_IMAGE_URL.replace('{{ratio}}', ratio).replace('{{image}}', scope.image.id);
+            } else {
+              scope.imageUrl = false;
+            }
+          });
+        }
+      };
+    }
+  ]);
