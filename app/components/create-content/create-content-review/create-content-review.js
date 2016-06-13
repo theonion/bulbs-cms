cddb022845793a67dec775265a8e131460cd9e27
Andrew Kos <akos123@gmail.com>
2016-06-13 14:58:28 -0500

    removed create-content-review

--- app/components/create-content/create-content-review/create-content-review.js
+++
@@ -1,6 +0,0 @@
-angular.module('bulbs.cms.content.review', [])
-  .directive('createContentReview', function () {
-    return {
-        template: '/create-content/create-content-review.html'
-    }
-  });
