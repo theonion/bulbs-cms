26bd3acf5b6d84ce0339c102b24c1640adab70be
Andrew Kos <akos123@gmail.com>
2016-06-14 11:55:16 -0500

    add path to utils, change utils to a provider for configs

--- app/shared/utils/utils.js
+++ app/shared/utils/utils.js
@@ -1,10 +1,9 @@
 'use strict';
 
 angular.module('bulbs.cms.utils', [])
-  .service('Utils', ['_', function (_) {
-    var Utils = this;
+  .provider('Utils', ['_', function (_) {
 
-    Utils.slugify = function (text) {
+    this.slugify = function (text) {
       // https://gist.github.com/mathewbyrne/1280286
       return text.toString().toLowerCase()
         .replace(/\s+/g, '-')           // Replace spaces with -
@@ -21,7 +20,7 @@ angular.module('bulbs.cms.utils', [])
      * @param {Number} indexTo - Index to move content to.
      * @returns {Boolean} true if content moved, false otherwise.
      */
-    Utils.moveTo = function (list, indexFrom, indexTo) {
+    this.moveTo = function (list, indexFrom, indexTo) {
       var ret = false;
 
       if (indexFrom >= 0 && indexFrom < list.length &&
@@ -35,7 +34,7 @@ angular.module('bulbs.cms.utils', [])
       return ret;
     };
 
-    Utils.removeFrom = function (list, index) {
+    this.removeFrom = function (list, index) {
       return list.splice(index, 1).length > 0;
     };
 
@@ -46,7 +45,7 @@ angular.module('bulbs.cms.utils', [])
     * @param {Object} params - Object of params to serialize.
     * @returns {String} query - a url querystring (is prefixed with '?')
     */
-    Utils.param = function (params) {
+    this.param = function (params) {
       if (!params) {
         params = {};
       }
@@ -62,5 +61,35 @@ angular.module('bulbs.cms.utils', [])
       return query;
     };
 
-    return Utils;
+    this.path = {
+      /**
+       * Join path strings.
+       *
+       * @param {...String} A variable number of strings to join into path.
+       * @returns {String} joined path.
+       */
+      join: function () {
+        var sep = '/';
+        var replace = new RegExp(sep + '{1,}', 'g');
+        var argsArr = Array.prototype.slice.call(arguments);
+        // if there's a protocol, make sure to ignore it when replacing sep
+        var protocolPrefix = '';
+        if (argsArr.length > 0 && typeof(argsArr[0]) === 'string') {
+          var matches = argsArr[0].match(/^(https?:)?\/\//);
+          if (matches) {
+            protocolPrefix = matches[0];
+            argsArr[0] = argsArr[0].replace(protocolPrefix, '');
+          }
+        }
+        return protocolPrefix + argsArr.join(sep).replace(replace, sep);
+      }
+    };
+
+    this.$get = [
+      function () {
+        return this;
+      }
+    ];
+
+    return this;
   }]);
