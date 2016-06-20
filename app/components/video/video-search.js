205c871a62e786ae653153484d09763238c4bab4
Dayton Nolan <daytonn@gmail.com>
2016-06-20 15:09:33 -0500

    Add video search

---
+++ app/components/video/video-search.js
@@ -0,0 +1,58 @@
+'use strict';
+
+angular.module('bulb.cms.video.videoSearch', [
+  'lodash',
+  'VideohubClient.api',
+  'VideohubClient.settings',
+  'uuid4'
+])
+  .directive('videoSearch', [
+    '_', 'uuid4',
+    function (_, uuid4) {
+      return {
+        restrict: 'E',
+        require: 'ngModel',
+        templateUrl: '/cms/partials/directives/video-search.html',
+        scope: {
+          label: '@videoSearchLabel',
+          onSelect: '&videoSearchLabel'
+        },
+        controller: [
+          '$scope', 'Video', 'VIDEOHUB_DEFAULT_CHANNEL',
+          function ($scope, Video, VIDEOHUB_DEFAULT_CHANNEL) {
+            $scope.videoChannel = VIDEOHUB_DEFAULT_CHANNEL;
+
+            $scope.itemDisplayFormatter = function (video) {
+              if (_.isObject(video)) {
+                return video.id + ' - ' + video.title;
+              }
+            };
+
+            $scope.searchVideos = function (query) {
+              return Video.$postSearch({
+                query: query,
+                channel: VIDEOHUB_DEFAULT_CHANNEL
+              });
+            };
+          }
+        ],
+        link: function (scope, elements, attrs, ngModelCtrl) {
+          scope.uuid = uuid4.generate();
+
+          if (ngModelCtrl) {
+            scope.ngModel = ngModelCtrl;
+
+            ngModelCtrl.$render = function () {
+              if (_.has(ngModelCtrl.$modelValue, 'title') && !scope.initialValue) {
+                scope.initialValue = scope.itemDisplayFormatter(ngModelCtrl.$modelValue);
+              }
+            };
+
+            scope.onSelect = function (selection) {
+              ngModelCtrl.$commitViewValue();
+            };
+          }
+        }
+      }
+    }
+  ]);
