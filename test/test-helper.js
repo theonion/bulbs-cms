d35f393b632ebeacdab805eb6e0bc8b2f79fc907
Andrew Kos <akos123@gmail.com>
2016-06-22 14:00:01 -0500

    add doc to directiveBuilder

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,9 +1,18 @@
 !(function (global) {
 
-  var directiveBuilder = function ($compile, $scope, html) {
+  /**
+   * Allows tests to control when a directive is built.
+   *
+   * @param {Scope} $scope - parent scope of new directive.
+   * @param {String} html - html to use to build new directive.
+   * @returns {function} when executed, will digest the directive and its html
+   *    and bind it to a new scope that is a child of given $scope.
+   */
+  var directiveBuilder = function ($scope, html) {
     return function () {
       var $directiveScope = $scope.$new();
-      var element = $compile(html)($directiveScope);
+      var element = angular.injector(['ng'])
+        .get('$compile')(html)($directiveScope);
       $directiveScope.$digest();
       return element.scope();
     };
