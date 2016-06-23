c8808efb5c8856b8ea555f61e6c409e0e5e49d54
Andrew Kos <akos123@gmail.com>
2016-06-23 16:11:52 -0500

    clean up page-form-field tests

--- app/components/page/page-form/page-form-field/page-form-field.tests.js
+++ app/components/page/page-form/page-form-field/page-form-field.tests.js
@@ -1,42 +1,28 @@
 'use strict';
 
 describe('Directive: pageForm', function () {
-  var sandbox;
+  var digestedScope;
+  var html;
 
   beforeEach(function () {
     module('bulbs.cms.page.form.field');
     module('jsTemplates');
 
-    sandbox = sinon.sandbox.create();
+    inject(function ($compile, $rootScope) {
+      var parentHtml = angular.element('<page-form><form></form></page-form>');
+
+      html = angular.element('<page-form-field></page-form-field>');
+      parentHtml.find('form').append(html);
+
+      digestedScope = window.testHelper.directiveBuilder($compile, $rootScope, parentHtml);
+    });
   });
 
-  afterEach(function () {
-    sandbox.restore();
-  });
+  it('should render a <page-form-field-text> when given a schema with a type of "text"', function () {
+    html.attr('schema', JSON.stringify({ type: 'text' }));
 
-  context('normal usage', function () {
-    var digestedScope;
-    var formName = 'pageForm';
-    var html;
-    var parentHtml;
+    digestedScope();
 
-    beforeEach(function () {
-      inject(function ($compile, $rootScope) {
-        parentHtml = angular.element('<page-form><form name="' + formName + '"></form></page-form>');
-
-        html = angular.element('<page-form-field></page-form-field>');
-        parentHtml.find('form').append(html);
-
-        digestedScope = window.testHelper.directiveBuilder($compile, $rootScope, parentHtml);
-      });
-    });
-
-    it('should render a <page-form-field-text> when given a schema with a type of "text"', function () {
-      html.attr('schema', JSON.stringify({ type: 'text' }));
-
-      digestedScope();
-
-      expect(html.find('page-form-field-text').length).to.equal(1);
-    });
+    expect(html.find('page-form-field-text').length).to.equal(1);
   });
 });
