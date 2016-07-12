d8b8340f4f5b2ae867ac7b25485fc66192fc9bd4
Andrew Kos <akos123@gmail.com>
2016-07-12 11:39:42 -0500

    input text for single line editor

--- app/components/dynamic-content/dynamic-content-form/dynamic-content-form-field/dynamic-content-form-field-text/dynamic-content-form-field-text.tests.js
+++ app/components/dynamic-content/dynamic-content-form/dynamic-content-form-field/dynamic-content-form-field-text/dynamic-content-form-field-text.tests.js
@@ -18,7 +18,7 @@ describe('Directive: dynamicContentFormFieldText', function () {
     });
   });
 
-  it('should render a single line editor by default', function () {
+  it('should render a text input by default', function () {
     var html =
       '<form>' +
         '<dynamic-content-form-field-text ' +
@@ -34,7 +34,8 @@ describe('Directive: dynamicContentFormFieldText', function () {
 
     var element = digest(html);
 
-    expect(element.find('onion-editor').attr('role')).to.equal('singleline');
+    expect(element.find('input').attr('type')).to.equal('text');
+    expect(element.find('onion-editor').length).to.equal(0);
   });
 
   it('should render a multi-line editor when given a schema with field_type long', function () {
@@ -56,7 +57,7 @@ describe('Directive: dynamicContentFormFieldText', function () {
     expect(element.find('onion-editor').attr('role')).to.equal('multiline');
   });
 
-  it('should render static html when given a schema with read_only', function () {
+  it('should render a readonly input field with read_only and no field_size', function () {
     var html =
       '<form>' +
         '<dynamic-content-form-field-text ' +
@@ -69,6 +70,28 @@ describe('Directive: dynamicContentFormFieldText', function () {
     $parentScope.name = 'title';
     $parentScope.ngModel = 'some text value';
     $parentScope.schema = { read_only: true };
+
+    var element = digest(html);
+
+    expect(element.find('input').attr('readonly')).to.equal('readonly');
+  });
+
+  it('should render static html when given a schema with read_only and long field_size', function () {
+    var html =
+      '<form>' +
+        '<dynamic-content-form-field-text ' +
+            'name="{{ name }}" ' +
+            'ng-model="ngModel" ' +
+            'schema="schema" ' +
+            '>' +
+        '</dynamic-content-form-field-text>' +
+      '</form>';
+    $parentScope.name = 'title';
+    $parentScope.ngModel = 'some text value';
+    $parentScope.schema = {
+      field_size: 'long',
+      read_only: true
+    };
 
     var element = digest(html);
 
