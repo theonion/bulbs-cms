eaa1aa6ffd237e53bbd748fbd18dd94e16a51d57
Andrew Kos <akos123@gmail.com>
2016-07-20 17:08:34 -0500

    more stuff

--- app/components/dynamic-content/dynamic-content-form/dynamic-content-form-field/dynamic-content-form-field-choice/dynamic-content-form-field-choice.tests.js
+++ app/components/dynamic-content/dynamic-content-form/dynamic-content-form-field/dynamic-content-form-field-choice/dynamic-content-form-field-choice.tests.js
@@ -1,5 +1,20 @@
 'use strict';
 
 describe('Directive: dynamicContentFormFieldChoice', function () {
-  
+  var $parentScope;
+  var digest;
+
+  beforeEach(function () {
+    module('bulbs.cms.dynamicContent.form.field.choice');
+    module('jsTemplates');
+
+    inject(function ($compile, $rootScope) {
+      $parentScope = $rootScope.$new();
+
+      digest = window.testHelper.directiveBuilderWithDynamicHtml(
+        $compile,
+        $parentScope
+      );
+    });
+  });
 });
