32c905b9c1875c0f8a0ef257cdc658918122d8d6
Andrew Kos <akos123@gmail.com>
2016-07-21 11:57:00 -0500

    add required an read_only

--- app/components/dynamic-content/dynamic-content-form/dynamic-content-form-field/dynamic-content-form-field-choice/dynamic-content-form-field-choice.tests.js
+++ app/components/dynamic-content/dynamic-content-form/dynamic-content-form-field/dynamic-content-form-field-choice/dynamic-content-form-field-choice.tests.js
@@ -100,4 +100,56 @@ describe('Directive: dynamicContentFormFieldChoice', function () {
 
     expect($parentScope.ngModel.shape).to.equal(1);
   });
+
+  it('should have required validation when given a schema with required', function () {
+    var choice1 = { display_name: 'Circle', value: 0 };
+    var choice2 = { display_name: 'Square', value: 1 };
+    var html =
+      '<form name="myForm">' +
+        '<dynamic-content-form-field-choice ' +
+            'name="{{ name }}" ' +
+            'ng-model="ngModel" ' +
+            'schema="schema" ' +
+            '>' +
+        '</dynamic-content-form-field-choice>' +
+      '</form>';
+    $parentScope.name = 'shape';
+    $parentScope.ngModel = { shape: null };
+    $parentScope.schema = {
+      label: 'Shape',
+      choices: [choice1, choice2],
+      type: 'choice',
+      required: true
+    };
+
+    var element = digest(html);
+
+    expect(element.scope().myForm[$parentScope.name].$error.required).to.equal(true);
+  });
+
+  it('should render a readonly input when given a schema with read_only', function () {
+    var choice1 = { display_name: 'Circle', value: 0 };
+    var choice2 = { display_name: 'Square', value: 1 };
+    var html =
+      '<form name="myForm">' +
+        '<dynamic-content-form-field-choice ' +
+            'name="{{ name }}" ' +
+            'ng-model="ngModel" ' +
+            'schema="schema" ' +
+            '>' +
+        '</dynamic-content-form-field-choice>' +
+      '</form>';
+    $parentScope.name = 'shape';
+    $parentScope.ngModel = { shape: null };
+    $parentScope.schema = {
+      label: 'Shape',
+      choices: [choice1, choice2],
+      type: 'choice',
+      read_only: true
+    };
+
+    var element = digest(html);
+
+    expect(element.find('select').attr('readonly')).to.equal('readonly');
+  });
 });
