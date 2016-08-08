7bd35a75ec944a4d12a1ad178b78ee7d8107e470
Andrew Kos <akos123@gmail.com>
2016-08-08 16:38:53 -0500

    hot fix for status filter configurations

--- app/components/status-filter/status-filter-directive.js
+++ app/components/status-filter/status-filter-directive.js
@@ -2,30 +2,9 @@
 
 angular.module('statusFilter.directive', [
   'bulbs.cms.site.config',
-  'contentServices.listService'
+  'contentServices.listService',
+  'statusFilter.config'
 ])
-  .provider('StatusFilterOptions', function (moment) {
-    var _statuses = [
-      {label: 'Draft', key: 'status', value: 'draft'},
-      {label: 'Awaiting Review', key: 'status', value: 'Waiting for Editor'},
-      {label: 'Published', key: 'before', value: function () { return moment().format('YYYY-MM-DDTHH:mmZ'); }},
-      {label: 'Scheduled', key: 'after', value: function () { return moment().format('YYYY-MM-DDTHH:mmZ'); }},
-      {label: 'All', key: null, value: null}
-    ];
-
-    this.setStatuses = function (statuses) {
-      _statuses = statuses;
-    };
-
-    this.$get = function () {
-      return {
-        getStatuses: function () {
-          return _statuses;
-        }
-      };
-    };
-
-  })
   .directive('statusFilter', function ($location, _, StatusFilterOptions, ContentListService, CmsConfig) {
     return {
       templateUrl: CmsConfig.buildComponentPath('status-filter/status-filter.html'),
