0af0f84065a6d59e5e45db51bc282842e9b43bda
Andrew Kos <akos123@gmail.com>
2016-08-12 11:56:15 -0500

    use status filter options for super features list page filters

--- app/components/super-features/super-features-list/super-features-list.js
+++ app/components/super-features/super-features-list/super-features-list.js
@@ -1,16 +1,22 @@
 'use strict';
 
 angular.module('bulbs.cms.superFeatures.list', [
-  'bulbs.cms.superFeatures.api',
   'bulbs.cms.site.config',
+  'bulbs.cms.superFeatures.api',
   'bulbsCmsApp.nonRestmodListPage',
+  'lodash',
   'moment',
-  'ngRoute'
+  'ngRoute',
+  'statusFilter.config'
 ])
   .config([
-    '$injector', '$routeProvider', 'CmsConfigProvider',
-    function ($injector, $routeProvider, CmsConfigProvider) {
+    '_', '$injector', '$routeProvider', 'CmsConfigProvider',
+      'StatusFilterOptionsProvider',
+    function (_, $injector, $routeProvider, CmsConfigProvider,
+        StatusFilterOptionsProvider) {
+
       var CmsConfig = $injector.invoke(CmsConfigProvider.$get);
+      var StatusFilterOptions = $injector.invoke(StatusFilterOptionsProvider.$get);
 
       $routeProvider
         .when('/cms/app/super-features/', {
@@ -23,6 +29,27 @@ angular.module('bulbs.cms.superFeatures.list', [
               $scope.editPageUrlBuilder = function (item) {
                 return '/cms/app/edit/' + item.id + '/' + (item.polymorphic_ctype || CmsConfig.getSuperFeaturesType());
               };
+
+              // TODO: using status filters from content page here since list
+              //  page needs them in a different format. when we move content
+              //  list page to the generic list page directive, this can be
+              //  removed.
+              $scope.statusFilterOptions = StatusFilterOptions.getStatuses()
+                .filter(function (option) {
+                  return option.label !== 'All';
+                })
+                .map(function (option) {
+                  var params = {};
+
+                  if (option.key) {
+                    params[option.key] = _.isFunction(option.value) ? option.value() : option.value;
+                  }
+
+                  return {
+                    title: option.label,
+                    params: params
+                  };
+                });
             }
           ],
           templateUrl: CmsConfig.buildComponentPath(
