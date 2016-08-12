e2b5a319e58793dbf91bee120256be320d7158ac
Andrew Kos <akos123@gmail.com>
2016-08-12 14:49:03 -0500

    add way to retrieve relations for a dynamic content piece

--- app/components/dynamic-content/dynamic-content-api/dynamic-content-api.js
+++ app/components/dynamic-content/dynamic-content-api/dynamic-content-api.js
@@ -1,14 +1,22 @@
 'use strict';
 
-angular.module('bulbs.cms.dynamicContent.api', [])
+angular.module('bulbs.cms.dynamicContent.api', [
+  'bulbs.cms.site.config'
+])
   .service('DynamicContentApi', [
-    '$http',
-    function ($http) {
+    '$http', 'CmsConfig',
+    function ($http, CmsConfig) {
       return {
         retrieveSchema: function (url) {
           return $http({
             method: 'OPTIONS',
             url: url
+          });
+        },
+        retrieveRelations: function (id) {
+          return $http({
+            method: 'GET',
+            url: CmsConfig.buildApiUrlRoot(id, 'relations')
           });
         }
       };
