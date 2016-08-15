5cb455cb48598d4491a79cee4788bbc100a716c4
Andrew Kos <akos123@gmail.com>
2016-08-15 12:16:23 -0500

    date time modal time selection fix (#182)
    * html cleanup
    
    * fix is in
    
    * add tests
    
    * fix set date
    
    * addressing concerns

--- app/components/date-time-modal/date-time-modal-controller.js
+++ app/components/date-time-modal/date-time-modal-controller.js
@@ -2,16 +2,30 @@
 
 angular.module('bulbs.cms.dateTimeModal.controller', [
   'bulbs.cms.site.config',
+  'moment',
   'ui.bootstrap.modal'
 ])
   .controller('DatetimeSelectionModalCtrl', [
     '$scope', '$modalInstance', 'CmsConfig', 'moment',
     function ($scope, $modalInstance, CmsConfig, moment) {
 
+      $scope.TIMEZONE_LABEL = moment.tz(CmsConfig.getTimezoneName()).format('z');
+
       // ensure that we can't choose a time if date is invalid
-      $scope.dateValid = false;
+      $scope.dateValid = true;
       $scope.$watch('tempDatetime', function () {
-        $scope.dateValid = moment($scope.tempDatetime).isValid();
+        $scope.dateValid = $scope.tempDatetime.isValid();
+      });
+
+      // watch temp time to update date
+      $scope.$watch('tempTime', function () {
+        var newTime = moment($scope.tempTime);
+
+        if (newTime.isValid()) {
+          $scope.tempDatetime.hour(newTime.hour());
+          $scope.tempDatetime.minute (newTime.minute());
+          $scope.tempDatetime.second(newTime.second());
+        }
       });
 
       // copy date temporarily so user has to actually verify change to the date
@@ -20,26 +34,28 @@ angular.module('bulbs.cms.dateTimeModal.controller', [
         // default to now if no time given
         $scope.tempDatetime = moment();
       }
-
-      $scope.TIMEZONE_LABEL = moment.tz(CmsConfig.getTimezoneName()).format('z');
+      $scope.tempTime = angular.copy($scope.tempDatetime);
 
       var timeNowWithOffset = function () {
         return moment.tz(CmsConfig.getTimezoneName());
       };
 
-      // callback function for using datetime calendar because it doesn't work at all in a sensible way
+      // callback function for using datetimepicker calendar because it doesn't
+      //  modify the given ngModel
       $scope.setDate = function (newDate) {
-        $scope.tempDatetime = moment(newDate);
+        var newDateAsMoment = moment(newDate);
+        $scope.tempDatetime = ($scope.tempDatetime || moment())
+          .year(newDateAsMoment.year())
+          .month(newDateAsMoment.month())
+          .date(newDateAsMoment.date());
       };
 
       $scope.setDateToday = function () {
-        var now = timeNowWithOffset();
-        $scope.tempDatetime = moment().year(now.year()).month(now.month()).date(now.date());
+        $scope.setDate(timeNowWithOffset());
       };
 
       $scope.setDateTomorrow = function () {
-        var now = timeNowWithOffset();
-        $scope.tempDatetime = moment().year(now.year()).month(now.month()).date(now.date() + 1);
+        $scope.setDate(timeNowWithOffset().add(1, 'date'));
       };
 
       $scope.setTimeNow = function () {
@@ -47,7 +63,10 @@ angular.module('bulbs.cms.dateTimeModal.controller', [
       };
 
       $scope.setTimeMidnight = function () {
-        $scope.tempDatetime = timeNowWithOffset().hour(24).minute(0);
+        $scope.tempDatetime = timeNowWithOffset()
+            .hour(24)
+            .minute(0)
+            .second(0);
       };
 
       $scope.chooseDatetime = function () {
