da5a6132785a6c3fe075b74230c4c1c0303a1007
Andrew Kos <akos123@gmail.com>
2016-08-15 17:51:11 -0500

    adding some relations content

--- app/components/super-features/super-feature-relations/super-feature-relations.js
+++ app/components/super-features/super-feature-relations/super-feature-relations.js
@@ -1,17 +1,25 @@
 'use strict';
 
 angular.module('bulbs.cms.superFeatures.relations', [
+  'bettyEditable',
   'bulbs.cms.site.config',
-  'bulbs.cms.superFeatures.api'
+  'bulbs.cms.superFeatures.api',
+  'statusFilter.config'
 ])
   .directive('superFeatureRelations', [
-    'CmsConfig', 'SuperFeaturesApi',
-    function (CmsConfig, SuperFeaturesApi) {
+    'CmsConfig', 'SuperFeaturesApi', 'StatusFilterOptions',
+    function (CmsConfig, SuperFeaturesApi, StatusFilterOptions) {
       return {
         scope: {
           article: '='
         },
         link: function (scope, element, attrs) {
+          scope.statuses = StatusFilterOptions.getStatuses()
+            .filter(function (status) {
+              // remove default status
+              return !!status.value;
+            });
+
           SuperFeaturesApi.getSuperFeatureRelations(scope.article.id)
             .then(function (response) {
               scope.relations = response.data.results;
