c5398c19b455e1c7ca133b31813eb380fdbde2cf
Cameron Lowe <cam.m.lowe@gmail.com>
2017-03-15 13:29:23 -0500

    Test flat_rate $dirty

--- app/shared/api-services/reporting/reporting-factory.tests.js
+++ app/shared/api-services/reporting/reporting-factory.tests.js
@@ -6,6 +6,7 @@ describe('Role Factory', function() {
 
   var Role;
 
+  var mockRolePayload;
   var role;
   var roleUrl;
 
@@ -16,15 +17,26 @@ describe('Role Factory', function() {
       $httpBackend = _$httpBackend_;
       $q = _$q_;
       Role = _Role_;
-      roleUrl = '/cms/api/v1/contributions/role';
+
+      roleUrl = '/cms/api/v1/contributions/role/';
+      mockRolePayload = {
+        id: 12345,
+        name: 'flat rate role',
+        payment_type: 'Flat Rate',
+        rate: 125
+      }
     }));
 
   describe('updateRole()', function () {
-
-      it('is dirty when flat_rate is altered.', function () {
-        role = Role.$build();
+      it('is dirty when flat_rate.rate is updated.', function () {
+        $httpBackend.expectGET(roleUrl + mockRolePayload.id + '/').respond(200, mockRolePayload);
+        role = Role.$find(mockRolePayload.id);
+        $httpBackend.flush();
         expect(role.flat_rate.rate).to.equal(0);
-        expect(role.$dirty().length).to.equal(0);
+        expect(role.$dirty('flat_rate.rate')).to.be.false;
+
+        role.flat_rate.rate = 10;
+        expect(role.$dirty('flat_rate.rate')).to.be.true;
       });
 
   });
